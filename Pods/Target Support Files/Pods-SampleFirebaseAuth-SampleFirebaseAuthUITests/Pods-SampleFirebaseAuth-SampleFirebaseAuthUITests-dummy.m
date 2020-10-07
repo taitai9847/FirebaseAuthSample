0bf8b38ebdf0c5c5f21d4bed40096008beae4ce6
taitai9847 <taitai9847@gmail.com>
2020-10-07 17:43:28 +0900

    initial commit

---
+++ Pods/Target Support Files/Pods-SampleFirebaseAuth-SampleFirebaseAuthUITests/Pods-SampleFirebaseAuth-SampleFirebaseAuthUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleFirebaseAuth_SampleFirebaseAuthUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleFirebaseAuth_SampleFirebaseAuthUITests
+@end
---
+++ SampleFirebaseAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFirebaseAuth
+//
+//  Created by 石川太洋 on 2020/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
